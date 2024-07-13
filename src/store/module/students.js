bc4cf68fed17a9202bb6136df2e1a274e7d9f0be
otimbrian <otims.brian@gmail.com>
2024-07-13 14:29:29 +0300

    Updated store to accomodate student updates

--- src/store/module/students.js
+++ src/store/module/students.js
@@ -14,13 +14,18 @@ const actions = {
         const students = await studentsService.getAllStudents()
         console.log(students)
         commit('initializeStudents', students)
-
-        console.log('....Done.....')
     },
 
     setStudent({ commit }, id) {
-        console.log('setting id ----->', id)
         commit('set_student', id)
+    },
+
+    async updateStudent({ commit }, student) {
+        console.log('Updating to this ---->', student);
+        const data = await studentsService.updateStudent(student)
+        console.log('Response after update ---->', data)
+
+        commit('update_student', student)
     }
 }
 
@@ -31,7 +36,12 @@ const mutations = {
 
     set_student(state, id) {
         state.student = state.all.find(stu => Number(stu.id) === Number(id))
-        console.log("Dones setting --->");
+    },
+
+    update_student(state, student) {
+        state.all = state.all.map(
+            stud => stud.id !== student.id ? stud : student
+        )
     }
 }
 
